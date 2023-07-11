2f7e23bb41a21caad4e9a6cb6d8ff6934319b328
Maarten Tamminga <maarten.tamminga@hotmail.com>
2023-07-11 14:59:30 +0200

    wip

---
+++ nam_core.cpp
@@ -0,0 +1,10 @@
+#include "nam_core.h"
+
+#include "NAM/dsp.cpp"
+#include "NAM/activations.cpp"
+#include "NAM/convnet.cpp"
+#include "NAM/lstm.cpp"
+#include "NAM/util.cpp"
+#include "NAM/version.cpp"
+#include "NAM/wavenet.cpp"
+#include "NAM/get_dsp.cpp"
---
+++ nam_core.h
@@ -0,0 +1,12 @@
+//Assume eigen is included already
+//Assume nlohmann json is included already
+
+#include "NAM/dsp.h"
+#include "NAM/activations.h"
+#include "NAM/convnet.h"
+#include "NAM/lstm.h"
+#include "NAM/util.h"
+#include "NAM/version.h"
+#include "NAM/wavenet.h"
+
+#include "dsp/dsp.h"
