559394a436f700ec41d2c347f3565e6c1a0ec56e
Lucas Baclé <lucas.bacle@orosys.fr>
2023-07-12 10:42:30 +0200

    fix missing definitions

--- nam_core_aio.cpp
+++ nam_core_aio.cpp
@@ -1,10 +1,7 @@
-#include "nam_core.h"
-
 #include "NAM/dsp.cpp"
 #include "NAM/activations.cpp"
 #include "NAM/convnet.cpp"
 #include "NAM/lstm.cpp"
 #include "NAM/util.cpp"
-#include "NAM/version.cpp"
 #include "NAM/wavenet.cpp"
 #include "NAM/get_dsp.cpp"
